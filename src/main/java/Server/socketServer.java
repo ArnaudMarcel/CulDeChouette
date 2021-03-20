99e19029ba2b0ece4f3fb4e3565eca45bed146f3
ArnaudMarcel <arnaud.lasv@gmail.com>
2021-03-20 11:14:11 +0100

    start

--- src/main/java/Server/socketServer.java
+++ src/main/java/Server/socketServer.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package web.main;
+package Server;
 
 /**
  *
  * @author darra
  */
-public class server {
+public class socketServer {
     
 }
