6ba2886ec0cc4da0f134d6a8eb8ca71faedff57a
ArnaudMarcel <arnaud.lasv@gmail.com>
2021-05-17 11:01:48 +0200

    modif compte/ historique

---
+++ target/generated-sources/annotations/Data/Lancer_.java
@@ -0,0 +1,19 @@
+package Data;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-05-12T08:41:45")
+@StaticMetamodel(Lancer.class)
+public class Lancer_ { 
+
+    public static volatile SingularAttribute<Lancer, Long> idPartie;
+    public static volatile SingularAttribute<Lancer, Long> idLancer;
+    public static volatile SingularAttribute<Lancer, Integer> valeurDes3;
+    public static volatile SingularAttribute<Lancer, Boolean> interaction;
+    public static volatile SingularAttribute<Lancer, Long> idJoueur;
+    public static volatile SingularAttribute<Lancer, Integer> valeurDes1;
+    public static volatile SingularAttribute<Lancer, Integer> valeurDes2;
+
+}
